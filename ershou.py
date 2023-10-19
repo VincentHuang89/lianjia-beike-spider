230d88b990f3643f148af85979bb7315ce868cc7
VincentHuang89 <709855497@qq.com>
2023-10-19 16:28:11 +0800

    ‘get more information of each real estate’

--- ershou.py
+++ ershou.py
@@ -2,11 +2,45 @@
 # coding=utf-8
 # author: Zeng YueTian
 # 此代码仅供学习与交流，请勿用于商业用途。
-# 获得指定城市的二手房数据
+# 获得指定城市的二手房数据gz
 
 from lib.spider.ershou_spider import *
+import pandas as pd
 
 if __name__ == "__main__":
+
     spider = ErShouSpider(SPIDER_NAME)
     spider.start()
 
+    '''
+    mes_list= []
+
+    
+    url = 'https://gz.ke.com/ershoufang/108403559345.html'
+    soup  = get_area_ershou_info_url(url)
+    mes_list.append(totalInform(url))
+
+    url ='https://gz.ke.com/ershoufang/108404922580.html'
+    soup  = get_area_ershou_info_url(url)
+    mes_list.append(totalInform(url))
+    print(mes_list)
+    DATA = pd.DataFrame(columns=['所在区域','小区名称','价格(万)','元/平方米','小区均价','标题描述','信息网址','房屋户型','建筑面积','户型结构','建筑类型','所在楼层','套内面积','房屋朝向','建筑结构','装修情况','梯户比例','配备电梯','挂牌时间','交易权属','上次交易','房屋用途','房屋年限','产权所属','抵押信息','房本备件','核心卖点','小区介绍','适宜人群','户型介绍','周边配套','税费解析','交通出行','户型特色','户型分间','30天看房记录','7天看房记录',])
+    for i in range(0,len(mes_list)):
+        data = pd.DataFrame(mes_list[i])
+        DATA = pd.concat([DATA,data],axis=0)
+    DATA.to_csv('test.csv')    
+
+    mes_str  = ['房屋户型','建筑面积','户型结构','建筑类型','所在楼层','套内面积','房屋朝向','建筑结构','装修情况','梯户比例','配备电梯']
+    house_elements = soup.find('div', class_="newwrap baseinform").find('div',class_='base').find_all('li',class_="")
+    print(house_elements)
+    mes1 = get_inform(house_elements, mes_str)
+    print(mes1)
+    '''
+    '''
+    url = 'https://gz.ke.com/ershoufang/108404883042.html'
+    mes1 = totalInform(url)
+    print(mes1)
+    ershou = spider.get_area_ershou_info('gz','linhe')
+    print(len(ershou))
+    print(ershou)
+    '''
--- lib/spider/ershou_spider.py
+++ lib/spider/ershou_spider.py
@@ -15,7 +15,7 @@ from lib.utility.path import *
 from lib.zone.area import *
 from lib.utility.log import *
 import lib.utility.version
-
+import pandas as pd
 
 class ErShouSpider(BaseSpider):
     def collect_area_ershou_data(self, city_name, area_name, fmt="csv"):
@@ -29,21 +29,26 @@ class ErShouSpider(BaseSpider):
         """
         district_name = area_dict.get(area_name, "")
         csv_file = self.today_path + "/{0}_{1}.csv".format(district_name, area_name)
-        with open(csv_file, "w") as f:
-            # 开始获得需要的板块数据
-            ershous = self.get_area_ershou_info(city_name, area_name)
+
+        ershous = self.get_area_ershou_info(city_name, area_name)
             # 锁定，多线程读写
-            if self.mutex.acquire(1):
-                self.total_num += len(ershous)
-                # 释放
-                self.mutex.release()
-            if fmt == "csv":
-                for ershou in ershous:
-                    # print(date_string + "," + xiaoqu.text())
-                    f.write(self.date_string + "," + ershou.text() + "\n")
+        if self.mutex.acquire(1):
+            self.total_num += len(ershous)
+            # 释放
+            self.mutex.release()
+        if fmt == "csv":
+            DATA = pd.DataFrame(columns=['所在区域','小区名称','价格(万)','元/平方米','小区均价','标题描述','信息网址','房屋户型','建筑面积','户型结构','建筑类型','所在楼层','套内面积','房屋朝向','建筑结构','装修情况','梯户比例','配备电梯','挂牌时间','交易权属','上次交易','房屋用途','房屋年限','产权所属','抵押信息','房本备件','核心卖点','小区介绍','适宜人群','户型介绍','周边配套','税费解析','交通出行','户型特色','户型分间','30天看房记录','7天看房记录',])
+            for i in range(0,len(ershous)):
+                data = pd.DataFrame(ershous[i])
+                DATA = pd.concat([DATA,data],axis=0)
+            DATA.to_csv(csv_file)    
+
         print("Finish crawl area: " + area_name + ", save data to : " + csv_file)
 
     @staticmethod
+
+
+
     def get_area_ershou_info(city_name, area_name):
         """
         通过爬取页面获得城市指定版块的二手房信息
@@ -83,27 +88,20 @@ class ErShouSpider(BaseSpider):
             BaseSpider.random_delay()
             response = requests.get(page, timeout=10, headers=headers)
             html = response.content
-            soup = BeautifulSoup(html, "lxml")
 
+            soup = BeautifulSoup(html, "lxml")
             # 获得有小区信息的panel
             house_elements = soup.find_all('li', class_="clear")
+            detail_urls =[]
+
             for house_elem in house_elements:
-                price = house_elem.find('div', class_="totalPrice")
-                name = house_elem.find('div', class_='title')
-                desc = house_elem.find('div', class_="houseInfo")
-                pic = house_elem.find('a', class_="img").find('img', class_="lj-lazy")
-
-                # 继续清理数据
-                price = price.text.strip()
-                name = name.text.replace("\n", "")
-                desc = desc.text.replace("\n", "").strip()
-                pic = pic.get('data-original').strip()
-                # print(pic)
-
-
-                # 作为对象保存
-                ershou = ErShou(chinese_district, chinese_area, name, price, desc, pic)
-                ershou_list.append(ershou)
+                #print('house_elem:',house_elem)
+                detail_url = house_elem.find('a')['href']
+                detail_urls.append(detail_url)
+            for url in detail_urls:
+                mes1 = totalInform(url)
+                ershou_list.append(mes1)
+        print(f'Total records{len(ershou_list)} in {city_name},{district_name},{area_name}')
         return ershou_list
 
     def start(self):
@@ -149,6 +147,137 @@ class ErShouSpider(BaseSpider):
         print("Total crawl {0} areas.".format(len(areas)))
         print("Total cost {0} second to crawl {1} data items.".format(t2 - t1, self.total_num))
 
+def get_area_ershou_info_url(url):
+    headers = create_headers()
+    response = requests.get(url, timeout=10, headers=headers)
+    html = response.content
+    soup = BeautifulSoup(html, "lxml")   
+    return soup
+
+def get_str(S:str,s1:str,s2:str):
+    '''
+    从字符串中截取两个标识字符之间的字符串
+    '''
+    i1 = S.find(s1)
+    i2 = S[i1:-1].find(s2)
+    if s2 =='':
+       return S[int(i1+len(s1)):len(S)]
+    else:
+        return S[int(i1+len(s1)):int(i1+i2)]
+
+
+
+def get_baseinform(soup):  
+    '''
+    获取房源基本信息('房屋户型','建筑面积','户型结构','建筑类型','所在楼层','套内面积','房屋朝向','建筑结构','装修情况','梯户比例','配备电梯')和交易信息('挂牌时间','交易权属','上次交易','房屋用途','房屋年限','产权所属','抵押信息','房本备件')
+    ''' 
+    mes_str  = ['房屋户型','建筑面积','户型结构','建筑类型','所在楼层','套内面积','房屋朝向','建筑结构','装修情况','梯户比例','配备电梯']
+    mes = {}
+    house_elements = soup.find('div', class_="newwrap baseinform").find('div',class_='base').find_all('li',class_="")
+    for i in range(0,len(house_elements)):
+        house_element = re.sub(' +', '', house_elements[i].text.strip()) 
+        house_element = house_element.replace('\n','')  #删除所有回车键
+        for s in mes_str:
+            if s in house_element:
+                mes[s]=get_str(house_element,s,'')
+
+
+    mes_str  = ['挂牌时间','交易权属','上次交易','房屋用途','房屋年限','产权所属','抵押信息','房本备件']
+    house_elements = soup.find('div', class_="newwrap baseinform").find('div',class_='transaction').find_all('li',class_="")
+    for i in range(0,len(house_elements)):
+        house_element = re.sub(' +', '', house_elements[i].text.strip()) 
+        house_element = house_element.replace('\n','')  #删除所有回车键
+        for s in mes_str:
+            if s in house_element:
+                mes[s]=get_str(house_element,s,'')
+
+
+    return mes
+
+
+
+
+def get_inform(house_elements, mes_str):  
+    mes = {}
+    for i in range(0,len(house_elements)):
+        house_element = re.sub(' +', '', house_elements[i].text.strip()) 
+        house_element = house_element.replace('\n','')  #删除所有回车键
+        for s in mes_str:
+            if s in house_element:
+                mes[s]=get_str(house_element,s,'')
+    return mes
+def get_layout(house_elements):
+    mes = []
+    for i in range(0,len(house_elements)):
+        house_element = re.sub(' +', '', house_elements[i].text.strip()) 
+        house_element = house_element.replace('\n','|')  
+
+        mes.append(house_element)
+    return mes
+
+
+def totalInform(url):
+    #print(url)
+    soup  = get_area_ershou_info_url(url)
+    mes1 = {}
+    mes_str  = ['房屋户型','建筑面积','户型结构','建筑类型','套内面积','房屋朝向','建筑结构','装修情况','梯户比例','配备电梯']
+    house_elements = soup.find('div', class_="newwrap baseinform").find('div',class_='base').find_all('li',class_="")
+    mes1 = get_inform(house_elements, mes_str)
+
+
+
+    mes_str  = ['挂牌时间','交易权属','上次交易','房屋用途','房屋年限','产权所属','抵押信息','房本备件']
+    house_elements = soup.find('div', class_="newwrap baseinform").find('div',class_='transaction').find_all('li',class_="")
+    mes1.update(get_inform(house_elements, mes_str))
+
+
+    mes_str  = ['核心卖点','小区介绍','适宜人群','户型介绍','周边配套','税费解析','交通出行']
+    house_elements = soup.find_all('div', class_="baseattribute clear")
+    mes1.update(get_inform(house_elements, mes_str))
+
+    house_elements = soup.find('div', class_="newwrap baseinform").find('div',class_='base').find_all('li',class_="oneline")
+    mes1['所在楼层']=house_elements[0].text.strip().replace('\n','').replace(' ','').replace('所在楼层','').replace('咨询楼层','')
+
+
+    mes_str  = ['小区名称']
+    house_elements = soup.find_all('div', class_="communityName")
+    mes = get_inform(house_elements, mes_str)
+    mes1['小区名称']= mes['小区名称'].replace('地图','')
+
+    mes_str  = ['所在区域']
+    house_elements = soup.find_all('div', class_="areaName")
+    mes1.update(get_inform(house_elements, mes_str))
+
+    house_elements = soup.find('div', class_="price").find_all('span',class_='total')
+    mes1['价格(万)']=house_elements[0].text.strip()
+
+    house_elements = soup.find('div', class_="unitPrice").find_all('span',class_='unitPriceValue')
+    mes1['元/平方米']=house_elements[0].text.strip()
+
+    house_elements = soup.find('div', class_="title").find_all('h1',class_='main')
+    mes1['标题描述']=house_elements[0].text.strip()
+
+    house_elements = soup.find('div', class_="xiaoqu_info").find_all('span',class_='xiaoqu_main_info price_red')
+    mes1['小区均价']=house_elements[0].text.strip()
+
+
+    house_elements = soup.find_all('div', class_="count")
+    mes1['7天看房记录']=house_elements[0].text.strip()
+
+    house_elements = soup.find_all('div', class_="totalCount")
+    mes1['30天看房记录']=house_elements[0].text.strip()
+
+    house_elements = soup.find_all('div', class_="fr list")
+    if len(house_elements)>0:
+        mes1['户型特色'] =house_elements[0].text.strip().replace('\n','').replace(' ','')
+
+    house_elements = soup.find_all('ul', id="layoutList")
+
+    mes1['户型分间'] = get_layout(house_elements)
+    mes1['信息网址']=url
+
+    return mes1
 
 if __name__ == '__main__':
+    
     pass
